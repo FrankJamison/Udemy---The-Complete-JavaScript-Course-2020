4182cdd617edfd937aa45165c1b12a86b8e6758b
Frank Jamison <42881082+FrankJamison@users.noreply.github.com>
2020-01-01 18:31:31 -0800

    Implementing the Module Pattern

---
+++ Project 02 - Budget Application/js/app.js
@@ -0,0 +1,24 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+    // Some code
+
+})();
+
+
+
+// UI CONTROLLER
+var UIController = (function () {
+
+    // Some code
+
+})();
+
+
+
+// APPLICATION CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+    // Some code
+
+})(budgetController, UIController);
