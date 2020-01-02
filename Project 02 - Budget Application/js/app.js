d95f39c1a4e0c9b80af98152e20e859f9ec4da9a
Frank Jamison <42881082+FrankJamison@users.noreply.github.com>
2020-01-01 19:03:21 -0800

    Setting up the First Event Listeners

--- Project 02 - Budget Application/js/app.js
+++ Project 02 - Budget Application/js/app.js
@@ -16,9 +16,33 @@ var UIController = (function () {
 
 
 
-// APPLICATION CONTROLLER
+// GLOBAL APPLICATION CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    // Some code
+    var ctrlAddItem = function () {
+
+        //TODO: 1. Get field input data
+
+        //TODO: 2. Add the item to the budget controller
+
+        //TODO: 3. Add the item to the user interface
+
+        //TODO: 4. Calculate the budget
+
+        //TODO: 5. Display the budget on the user interface
+
+    }
+
+    // Add button event listener
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // Enter key event listener
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
